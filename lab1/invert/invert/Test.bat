1fa67694dddfab54d08eff186d0bddbd526a5d47
Sancene <56795876+Sancene@users.noreply.github.com>
2020-03-30 18:24:20 +0300

    invert added

---
+++ lab1/invert/invert/Test.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set PROGRAM="%~1"
+
+::Empty file
+%PROGRAM% Empty.txt > nul && goto err
+echo Test 1 passed
+
+::No arguments
+%PROGRAM% matrix3.txt > nul && goto err
+echo Test 2 passed
+
+::Normal work
+%PROGRAM% matrix.txt > "%TEMP%\output.txt" || goto err
+fc Output1.txt "%TEMP%\output.txt" > nul || goto err
+echo Test 3 passed
+
+::Invalid arguments count
+%PROGRAM% > nul && goto err
+echo Test 4 passed
+
+::Missing file
+%PROGRAM% FileThatDoesntExist.txt "%TEMP%\output.txt" > nul && goto err
+echo Test 5 passed
+
+::Normal work
+%PROGRAM% matrix2.txt > "%TEMP%\output.txt" || goto err
+fc Output2.txt "%TEMP%\output.txt" > nul || goto err
+echo Test 6 passed
+
+::Determinator is 0
+%PROGRAM% matrix4.txt "%TEMP%\output.txt" > nul && goto err
+echo Test 7 passed
+
+echo tests passed
+exit /B 0
+
+:err
+echo Test failed
+exit /B 1
--- lab1/invert/invert/invert.cpp
+++ lab1/invert/invert/invert.cpp
@@ -1,33 +1,31 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
-#include <algorithm>
+//#include <vector>
 #include <sstream>
-#include <iomanip>
 
 const int MATRIX_SIZE = 3;
 
 void Transpose(double matrix[MATRIX_SIZE][MATRIX_SIZE]) {
-	int tmp;
+	double temp;
 	for (int i = 0; i < MATRIX_SIZE; i++)
 		for (int j = i; j < MATRIX_SIZE; j++)
 		{
-			tmp = matrix[i][j];
+			temp = matrix[i][j];
 			matrix[i][j] = matrix[j][i];
-			matrix[j][i] = tmp;
+			matrix[j][i] = temp;
 		}
 }
 
 double FindDeterminant(const double matrix[MATRIX_SIZE][MATRIX_SIZE])
 {
-	double determinant;
-	determinant = matrix[0][0] * matrix[1][1] * matrix[2][2]
+	double determinant = matrix[0][0] * matrix[1][1] * matrix[2][2]
 		+ matrix[1][0] * matrix[0][2] * matrix[2][1]
 		+ matrix[2][0] * matrix[0][1] * matrix[1][2]
 		- matrix[2][0] * matrix[1][1] * matrix[0][2]
 		- matrix[1][0] * matrix[0][1] * matrix[2][2]
 		- matrix[0][0] * matrix[2][1] * matrix[1][2];
+
 	return determinant;
 }
 
@@ -41,7 +39,7 @@ void FindAdditions(double matrix[MATRIX_SIZE][MATRIX_SIZE], double additionsMatr
 	additionsMatrix[1][1] = (matrix[0][0] * matrix[2][2] - matrix[2][0] * matrix[0][2]) * determinant;
 	additionsMatrix[1][2] = ((-1) * (matrix[0][0] * matrix[2][1] - matrix[2][0] * matrix[0][1])) * determinant;
 
-	additionsMatrix[2][0] = (matrix[0][1] * matrix[1][2] - matrix[1][1] * matrix[0][2])* determinant;
+	additionsMatrix[2][0] = (matrix[0][1] * matrix[1][2] - matrix[1][1] * matrix[0][2]) * determinant;
 	additionsMatrix[2][1] = ((-1) * (matrix[0][0] * matrix[1][2] - matrix[1][0] * matrix[0][2])) * determinant;
 	additionsMatrix[2][2] = (matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]) * determinant;
 }
@@ -84,40 +82,44 @@ bool ReadMatrixFromFile(const std::string& inputFileName, double matrix[MATRIX_S
 
 bool InvertMatrix(double matrix[MATRIX_SIZE][MATRIX_SIZE])
 {
-	double determinant = 0;
-	determinant = FindDeterminant(matrix);
+	double determinant = FindDeterminant(matrix);
 	if (determinant == 0)
 	{
-		std::cout << "Determinant is 0. This matrix cannot be inverted \n";
+		std::cout << "Matrix with determinant equaling 0 cannot be inversed \n";
 		return true;
 	}
+
 	Transpose(matrix);
 	double additionMatrix[MATRIX_SIZE][MATRIX_SIZE];
 	FindAdditions(matrix, additionMatrix,  1 / determinant);
+
 	for (int i = 0; i < 3; ++i)
 	{
-		for (int j = 0; j < 3; ++j) {
+		for (int j = 0; j < 3; ++j) 
+		{
+			if (additionMatrix[i][j] == -0) 
+			{
+				additionMatrix[i][j] = 0;
+			}
 			matrix[i][j] = additionMatrix[i][j];
 		}
 	}
+
 	return true;
 }
 
 void PrintMatrix(const double matrix[MATRIX_SIZE][MATRIX_SIZE])
 {
+	std::cout.precision(3);
+	std::cout.setf(std::ios::fixed);
 	for (int i = 0; i < MATRIX_SIZE; i++)
 	{
 		for (int j = 0; j < MATRIX_SIZE; j++)
 		{
-			if (j < 2)
-			{
-				std::cout << std::setprecision(3) << matrix[i][j] << "\t";
-			}
-			else
-			{
-				std::cout << std::setprecision(3) << matrix[i][j] << '\n';
-			}
+
+			std::cout << matrix[i][j] << "\t";
 		}
+		std::cout << "\n";
 	}
 }
 
