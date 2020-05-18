5bac56dc33e8260263e82f865970aa5dff59a628
Sancene <56795876+Sancene@users.noreply.github.com>
2020-05-18 16:53:06 +0300

    lab2 minor fixes

--- lab2/html_decode/html_decode/html_decode.cpp
+++ lab2/html_decode/html_decode/html_decode.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main()
 {
-	std::string line;
-	while (getline(std::cin, line))
-	{
-		std::string decodedString = HtmlDecode(line);
-		std::cout << decodedString << endl;
-	}
+    std::string line;
+    while (getline(std::cin, line))
+    {
+	    std::string decodedString = HtmlDecode(line);
+	    std::cout << decodedString << endl;
+    }
     return 0;
 }
